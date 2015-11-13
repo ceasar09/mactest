565837d2d2b55b281b409a7184bcd14554bb8c3c
Alvin <AlvinChang@alvindembp.lan>
2015-11-13 23:46:11 +0800

    project created & test github

---
+++ socketPractise/socketPractise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  socketPractise
+//
+//  Created by Alvin on 15/11/13.
+//  Copyright © 2015年 Alvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
